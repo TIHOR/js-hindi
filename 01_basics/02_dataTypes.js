2cb7cf3fb82b4192723e4cf969566825807cfc73
TIHOR <rohitewari@gmail.com>
2024-10-18 04:26:14 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict"; // treat all JS code as newer js
+
+// alert(3+3)  // we are using nodeJs not browser
+
+console.log( 3 + 3)
+// code readability should be high
+
+// for documentation refer to MDN or tc39.es ECMAScript language spesification
+
+console.log("Rohit")
+
+let name = "rohit"
+let age = 19
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power 53 
+// bigInt used in stock market, redit, fb  application
+// string => ""
+// boolean => true/false
+// null is a standalone value
+// undefined =>
+// symbol => unique
+
+// object 
+
+//console.log(typeof "rohit");
+//console.log(typeof age);
+
+console.log(typeof null); // object
+console.log(typeof undefined);// undefined
+
+
+
