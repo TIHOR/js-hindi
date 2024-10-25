fea07454d5c63f355d980b761e5bb4b5c7a4a17f
TIHOR <rohitewari@gmail.com>
2024-10-25 16:08:46 +0530

    Memory Stack Heap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -35,8 +35,36 @@ const myFunction = function(){
     console.log("Hello World");    
 }
 
-console.log(typeof bigNumber);
-console.log(typeof myFunction);
-console.log(typeof heroes);
+// console.log(typeof bigNumber);
+// console.log(typeof myFunction);
+// console.log(typeof heroes);
+
+
+// --------------------------------------
+
+// Stack (Primitive) pass by value get a copy, Heap (Non Primitive) pass by reference
+
+let myYoutubeName ="joeydukersuty"
+let anothername = myYoutubeName
+console.log(anothername);
+
+anothername = "chaiaurcode"
+
+console.log(myYoutubeName);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@upi.com"
+}
+
+let userTwo = userOne
+
+userTwo.email = "rotew@ikea.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
 
 
