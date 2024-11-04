ddbd52e9ea34299ea7fd9504b89ba1ea8a3378c8
TIHOR <rohitewari@gmail.com>
2024-11-04 11:48:58 +0530

    Strings Operations

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,48 @@
+const name = "rohit"
+const repCount = 50
+
+//console.log(name + repCount + " Value");
+
+console.log(`Hello my name is ${name} and my repocount is ${repCount}`);
+
+const gameName = new String('tihor_rt')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+//console.log(gameName.charAt(2));
+
+//console.log(gameName.indexOf('o'));
+
+//console.log(gameName);
+
+const newString = gameName.substring(0,4)
+
+//console.log(newString);
+
+const anotherString = gameName.slice(-8,4)
+//console.log(anotherString);// tihor_rt
+
+
+const newString1 = "   rohit   "
+console.log(newString1);
+console.log(newString1.trim());
+
+const url = "https://lcorotew.com/rote%20tewari"
+
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('rote'))
+
+
+console.log(gameName.split('_'));
+
+
+
+
+
+
+
