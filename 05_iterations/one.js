67fd5114a1e4718906cda78a2a8834da50380470
TIHOR <rohitewari@gmail.com>
2024-11-18 11:42:25 +0530

    iterations - for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,48 @@
+// for
+
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    if (element == 5){
+        //console.log("5 is best nymber")
+    }
+    //console.log(element);    
+}
+
+//console.log(element);
+
+for (let i = 1; i <= 10; i++) {
+    //console.log(`Table of ${i}`);    
+    for (let j = 1; j <= 10; j++) {
+        //console.log(`inner loop value j : ${j} and inner loop value i : ${i}`);        
+        //console.log(`${i} * ${j} = ${i*j}`);
+        
+
+    }
+    
+}
+
+ // Break
+
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5 ) {
+//         console.log(`detected 5`);
+//         break                
+//     }
+//     console.log(`value of i is ${index}`);    
+// }
+
+
+
+// continue
+
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5 ) {
+//         console.log(`detected 5`);
+//         continue                
+//     }
+//     console.log(`value of i is ${index}`);    
+// }
+
+
+
+
